f198ce07f844a0b8807a36f51d3e13efe6eb6ce8
Mike Gnandt <mikegnandt@gmail.com>
2012-09-28 00:00:37 -0400

    initial commit

---
+++ MigrationVSIX/MigrationItemTemplate/Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentMigrator;
+
+namespace $rootnamespace$
+{
+  [Migration($migrationtimestamp$)]
+  public class $migrationclassname$ : Migration
+  {
+    public override void Up()
+    {
+    }
+
+    public override void Down()
+    {
+    }
+  }
+}
---
+++ MigrationVSIX/MigrationItemTemplateWizard/MigrationWizard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace MigrationItemTemplateWizard
+{
+  public class MigrationWizard : IWizard
+  {
+    public void BeforeOpeningFile(ProjectItem projectItem)
+    {
+    }
+
+    public void ProjectFinishedGenerating(Project project)
+    {
+    }
+
+    public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+    {
+    }
+
+    public void RunFinished()
+    {
+    }
+
+    public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+    {
+      var timestamp = DateTime.Now.ToString("yyyyMMddhhmmss");
+      replacementsDictionary["$migrationtimestamp$"] = timestamp;
+      replacementsDictionary["$migrationclassname$"] = replacementsDictionary["$safeitemname$"];
+      replacementsDictionary["$migrationfilename$"] = String.Format("{0}_{1}.cs", timestamp, replacementsDictionary["$safeitemname$"]);
+    }
+
+    public bool ShouldAddProjectItem(string filePath)
+    {
+      return true;
+    }
+  }
+}
